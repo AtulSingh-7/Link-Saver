0c3456840bd36121edca569287b72d69acd06902
AtulSingh-7 <atulnara5@gmail.com>
2025-07-15 11:18:37 +0530

    working

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import API from '../api';
 import BookmarkCard from '../components/BookmarkCard';
 import Navbar from '../components/Navbar';
 
 function Dashboard() {
+  const navigate = useNavigate();
   const [url, setUrl] = useState('');
   const [bookmarks, setBookmarks] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
 
   const fetchBookmarks = async () => {
     const res = await API.get('/bookmarks');
     setBookmarks(res.data);
   };
+
 
   useEffect(() => {
     fetchBookmarks();
